7d5b4fc39ebe96037fbfbe9578dfc6508e567308
Vladyslav Sitalo <sitalov@amazon.com>
2018-12-08 21:55:53 +0000

    A bit less dirty and a bit more functional version

--- podcast.py
+++ podcast.py
@@ -1,19 +1,17 @@
-from __future__ import unicode_literals
-
+import random
 import sys
 
-sys.path.insert(0, './.vendor')
+from newspaper import Article, ArticleException
+
+sys.path.insert(0, './_build')
 
 import logging
 
 import feedparser
-import datetime
 import dateutil.parser
 import hashlib
-from contextlib import closing
 from feedgen.feed import FeedGenerator
 from botocore.exceptions import BotoCoreError
-from bs4 import BeautifulSoup
 
 from fleece import boto3
 from fleece.xray import (monkey_patch_botocore_for_xray)
@@ -24,61 +22,53 @@ logging.getLogger("boto3").setLevel(logging.WARNING)
 monkey_patch_botocore_for_xray()
 
 MAX_TPS = 10
-MAX_CONCURENT_CONNECTIONS = 20
+MAX_CONCURRENT_CONNECTIONS = 20
 TASKS = 100
-REQUEST_LIMIT = 1200
+REQUEST_LIMIT = 99900  # https://docs.aws.amazon.com/polly/latest/dg/limits.html
 
 
-def split_content_by_dot(soup, max_len):
-    """
-    split HTML soup into parts not bigger than max_len may break prosody where
-    dot is not at the end of the sentence (like "St. Louis") in some cases may
-    be synthesized as two separate sentences
-    """
-    text = soup.get_text(" ", strip=True)
-    start = 0
-    while start < len(text):
-        if len(text) - start <= max_len:
-            yield text[start:]
-            return
-        max = start + max_len
-        index = text.rfind(".", start, max)
-        if index == start:
-            start += 1
-        elif index < 0:
-            yield text[start:max]
-            start = max
-        else:
-            yield text[start:index]
-            start = index
+def article_from_entry(entry):
+    article = Article(entry.link)
+
+    if "content" in entry:
+        article.set_html(entry.content[0].value)
+        logging.info("Using inline content")
+    else:
+        logging.info(f"Getting content from: {entry.link}")
+        article.download()
+
+    article.parse()
+    logging.debug("Just retrieved the following article: ")
+    logging.debug(article)
+    return article
 
 
-def get_entries(feed):
-    NEW_POST = u"""New post, author {author}, title {title} {content}"""
+def get_entries(feed: feedparser.FeedParserDict):
     for entry in feed.entries:
-        if "http" in entry.id:
-            nid = hashlib.md5(str(entry.id).encode("utf-8"))
-            entry.id = nid.hexdigest()
-        entry_content = entry.content[0].value
-        soup = BeautifulSoup(entry_content, 'html.parser')
-        chunks = split_content_by_dot(soup, REQUEST_LIMIT - len(NEW_POST))
-        chunks = list(chunks)
-        published = dateutil.parser.parse(entry.published)
-        for i, chunk in enumerate(chunks):
-            if i == 0:
-                chunk = NEW_POST.format(
-                    author=entry.author,
-                    title=entry.title,
-                    content=chunk)
+        try:
+            update_entry_id(entry)
+
+            article = article_from_entry(entry)
+            authors = " and ".join(article.authors)
+
+            # todo ssml for things
             yield dict(
-                content=chunk,
-                id="%s_%d" % (entry.id, i),
+                content=f"{entry.title} by {entry.get('author', authors)}. Published on {entry.published}.\n { article.text }",
+                id=entry.id,
                 title=entry.title,
-                published=published - datetime.timedelta(0, i),
+                published=(dateutil.parser.parse(entry.published)),
             )
+        except ArticleException as e:
+            logging.error(e)
 
 
-def lambda_handler(event, context):
+def update_entry_id(entry):
+    if "http" in entry.id:
+        nid = hashlib.md5(str(entry.id).encode("utf-8"))
+        entry.id = nid.hexdigest()
+
+
+def lambda_handler(event, _):
     rss = event['rss']
     bucket_name = event['bucket']
     logging.info(f"Processing url: {rss}")
@@ -88,39 +78,67 @@ def lambda_handler(event, context):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
 
-    logging.info("getting list of existing objects in the given bucket")
-    files = set(o.key for o in bucket.objects.all())
+    files = get_files_grouped_by_prefix(bucket)
 
     feed = feedparser.parse(rss)
 
+    feed_generator = init_feed_generator(feed)
+
+    for entry in get_entries(feed):
+        try:
+            # todo consider reading the generated feed first to get all the info from it instead of
+
+            id_prefix = entry['id']
+
+            file_name = files.get(id_prefix)
+
+            voice_id = get_voice_id()  # todo get from filename/metadata
+
+            feed_entry = feed_generator.add_entry()
+            feed_entry.id(entry['id'])
+            feed_entry.title(entry['title'] + f". Read by {voice_id}")
+            feed_entry.published(entry['published'])
+
+            if file_name is None:
+                file_prefix = f"{id_prefix}.{voice_id}"
+                response = polly.start_speech_synthesis_task(
+                    Text=entry['content'],
+                    OutputFormat="mp3",
+                    OutputS3BucketName=bucket_name,
+                    OutputS3KeyPrefix=file_prefix,
+                    VoiceId=voice_id
+                )
+
+                file_name = f"{file_prefix}.{response['SynthesisTask']['TaskId']}.mp3"
+            else:
+                logging.info(f"{file_name} is found. Skipping an entry with corresponding id")
+
+            entry_url = f"http://{bucket_name}.s3.amazonaws.com/{file_name}"  # todo mp3 is hardcoded
+            feed_entry.enclosure(entry_url, 0, 'audio/mpeg')
+        except BotoCoreError as error:
+            logging.error(error)
+
+    bucket.put_object(Key='podcast.xml', Body=feed_generator.rss_str(pretty=True))
+
+
+def get_files_grouped_by_prefix(bucket):
+    logging.info("getting list of existing objects in the given bucket")
+    files = (o.key for o in bucket.objects.all())
+
+    return {file.split('.')[0]: file for file in files}
+
+
+def get_voice_id():
+    # todo how do I prioritize some?
+    return random.choice(["Salli", "Salli", "Joey", "Justin", "Matthew", "Ivy", "Joanna", "Kendra", "Kimberly", "Amy",
+                          "Emma", "Brian", "Geraint", "Nicole", "Russell"])
+
+
+def init_feed_generator(feed):
     title = feed['feed']['title']
     feed_generator = FeedGenerator()
     feed_generator.load_extension('podcast')
-    feed_generator.title('Audio podcast based on: %s' % title)
+    feed_generator.title(f'Audio podcast based on: {title}')
     feed_generator.link(href=feed.feed.link, rel='alternate')
     feed_generator.subtitle(feed.feed.description)
-
-    for entry in get_entries(feed):
-        filename = "%s.mp3" % entry['id']
-        feed_entry = feed_generator.add_entry()
-        feed_entry.id(entry['id'])
-        feed_entry.title(entry['title'])
-        feed_entry.published(entry['published'])
-        entry_url = f"http://{bucket_name}.s3.amazonaws.com/{filename}"
-        feed_entry.enclosure(entry_url, 0, 'audio/mpeg')
-        if filename in files:
-            logging.info(f'Article "{entry["title"]}" with id {entry["id"]} already exist, skipping.')
-            continue
-        try:
-            logging.info(f"Next entry, size: {len(entry['content']):d}")
-            logging.debug(f"Content: {entry['content']}")
-            response = polly.synthesize_speech(
-                Text=entry['content'],
-                OutputFormat="mp3",
-                VoiceId="Salli"
-            )
-            with closing(response["AudioStream"]) as stream:
-                bucket.put_object(Key=filename, Body=stream.read())
-        except BotoCoreError as error:
-            logging.error(error)
-    bucket.put_object(Key='podcast.xml', Body=feed_generator.rss_str(pretty=True))
+    return feed_generator
